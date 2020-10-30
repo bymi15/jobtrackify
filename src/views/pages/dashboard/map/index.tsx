cf487157169eeb93cda5975822dc88c71b33acc1
Brian Min <bymi15@yahoo.com>
2020-10-30 03:50:22 +0000

    feat(dashboard): add map and statistic pages

---
+++ src/views/pages/dashboard/map/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+interface Props {}
+
+const Map: React.FC<Props> = () => {
+  return null;
+};
+
+export default Map;
--- src/views/routes/routes.ts
+++ src/views/routes/routes.ts
@@ -5,6 +5,10 @@ import { RouteComponent } from '../../store/types';
 import Register from '../pages/auth/Register';
 const Dashboard = async(() => import('../pages/dashboard'));
 const DashboardBoard = async(() => import('../pages/dashboard/board'));
+const DashboardMap = async(() => import('../pages/dashboard/map'));
+const DashboardStatistics = async(() =>
+  import('../pages/dashboard/statistics')
+);
 
 const landingRoute: RouteComponent = {
   path: '/',
@@ -21,15 +25,15 @@ const dashboardBoardRoute = {
   component: DashboardBoard,
 };
 
-// const dashboardMapRoute = {
-//   path: '/dashboard/map',
-//   component: DashboardMap,
-// };
+const dashboardMapRoute = {
+  path: '/dashboard/map',
+  component: DashboardMap,
+};
 
-// const dashboardStatsRoute = {
-//   path: '/dashboard/stats',
-//   component: DashboardStats,
-// };
+const dashboardStatisticsRoute = {
+  path: '/dashboard/statistics',
+  component: DashboardStatistics,
+};
 
 const loginRoute = {
   path: '/auth/login',
@@ -48,6 +52,6 @@ export const authRoutes: RouteComponent[] = [loginRoute, registerRoute];
 export const dashboardRoutes: RouteComponent[] = [
   dashboardRoute,
   dashboardBoardRoute,
-  //   dashboardMapRoute,
-  //   dashboardStatsRoute,
+  dashboardMapRoute,
+  dashboardStatisticsRoute,
 ];
