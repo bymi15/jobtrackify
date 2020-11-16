8e149c5e69f500eb7a5e9403f8d9f24f37fc745f
Brian Min <bymi15@yahoo.com>
2020-11-16 19:29:51 +0000

    refactor(composeProviders): modify composeProviders util function to allow provider props

--- src/utils/composeProviders.tsx
+++ src/utils/composeProviders.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 
 const composeProviders = (...Providers: any) => (Child: any) => (props: any) =>
-  Providers.reduce(
-    (acc: any, Provider: any) => <Provider>{acc}</Provider>,
-    <Child {...props} />
-  );
+  Providers.reduce((acc: any, cur: any) => {
+    const [Provider, providerProps] = Array.isArray(cur)
+      ? [cur[0], cur[1]]
+      : [cur, {}];
+    return <Provider {...providerProps}>{acc}</Provider>;
+  }, <Child {...props} />);
 
 export default composeProviders;
