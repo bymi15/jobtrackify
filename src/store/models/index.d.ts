b0e56c28488ad5e14e3858d75a036a5b1ddd7f74
Brian Min <bymi15@yahoo.com>
2020-11-16 19:31:50 +0000

    chore: update store models

--- src/store/models/index.d.ts
+++ src/store/models/index.d.ts
@@ -1,7 +1,7 @@
 import { IUser, IUserUpdate } from './IUser';
 import { IBoard } from './IBoard';
 import IBoardColumn from './IBoardColumn';
-import { IJob, IJobInput } from './IJob';
+import { IJob, IJobInput, IJobUpdate } from './IJob';
 import ICompany from './ICompany';
 import { INote, INoteInput, INoteUpdate } from './INote';
 
@@ -12,6 +12,7 @@ export {
   IBoardColumn,
   IJob,
   IJobInput,
+  IJobUpdate,
   ICompany,
   INote,
   INoteInput,
